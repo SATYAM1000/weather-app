cd89027c7a4c1d48c41334e35bf6689a40531ace
SATYAM1000 <satyamsingh748846@gmail.com>
2023-05-22 00:47:09 +0530

    API Changed

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const humidity=document.getElementById("humidity")
 const wind_speed=document.getElementById("wind-speed")
 
 async function checkWeather(city){
-    const api_key="96f768c2bfeb7da5c25b4d40df85f4ab";
+    const api_key="xyz";
     const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`
 
     const weather_data=await fetch(`${url}`).then(response=> response.json());
